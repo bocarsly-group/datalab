a86894578ab2084ee3f3eb764b724c64ec43ea29
Josh Bocarsly <32345545+jdbocarsly@users.noreply.github.com>
2024-02-12 13:21:06 -0600

    Update pydatalab/pydatalab/routes/v0_1/items.py
    Co-authored-by: Matthew Evans <7916000+ml-evs@users.noreply.github.com>

--- pydatalab/pydatalab/routes/v0_1/items.py
+++ pydatalab/pydatalab/routes/v0_1/items.py
@@ -86,18 +86,8 @@ def get_equipment_summary():
 
     _project = {
         "_id": 0,
-        "creators": {
-            "display_name": 1,
-            "contact_email": 1,
-        },
-        # "collections": {
-        #     "collection_id": 1,
-        #     "title": 1,
-        # },
         "item_id": 1,
         "name": 1,
-        "nblocks": {"$size": "$display_order"},
-        "characteristic_chemical_formula": 1,
         "type": 1,
         "date": 1,
         "refcode": 1,
